bcd55626f27edb4881dab06827eebb8dbee061be
Thomas Mouchelet <mouchelet.thomas@gmail.com>
2022-02-18 11:55:03 +0100

    delete

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,12 +2,12 @@ import { AiFillLike } from "react-icons/ai";
 
 function Message ({message, setMessagesData, index, messagesData}){
 
-    const deleteMessage = (index) => {
+    const deleteMessage = () => {
         messagesData.splice(index, 1)
         setMessagesData([...messagesData])
     }
 
-    const handleClick = () => {
+    const handleLike = () => {
         
     }
 
@@ -15,8 +15,8 @@ function Message ({message, setMessagesData, index, messagesData}){
         <div className="message">
             <strong>{message.pseudo} :</strong>
             <span>{message.message}</span>
-            <button onClick={() => deleteMessage(index)}>DELETE</button>
-            <button onClick={handleClick}><AiFillLike /> ({message.likes ? message.likes : "0"})</button>
+            <button onClick={deleteMessage}>DELETE</button>
+            <button onClick={handleLike}><AiFillLike /> ({message.likes ? message.likes : "0"})</button>
         </div>
     )
 }
